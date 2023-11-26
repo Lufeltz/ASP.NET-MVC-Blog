7ae65d0e2732910aaeae8f240ada9547c7b04806
Lufeltz <lufelt@proton.me>
2023-11-26 00:13:43 -0300

    feat: Adicionar rota get para ação de adicionar tags

--- Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie.Web/Controllers/AdminTagsController.cs
@@ -4,7 +4,8 @@ namespace Bloggie.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Add()
         {
             return View();
         }
